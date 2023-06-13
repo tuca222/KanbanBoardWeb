1a6261cd16528820cf75e6612b5ceef71efc0535
Tuca <tucaa222@gmail.com>
2023-06-13 18:49:14 -0300

    passando as rotas para app.ts

--- src/Api/app.ts
+++ src/Api/app.ts
@@ -1,8 +1,35 @@
+require('dotenv').config();
 const express = require('express');
-import { router } from './routes';
+const cors = require("cors");
+const session = require('express-session')
+import { createUserController } from '../Application/UseCases/CreateUser';
 
+//APP EXPRESS
 const app = express();
+
+//CONFIGURANDO MIDDLEWARES
+app.use(cors({ credentials: true, origin: true }));
+app.use(session({
+  secret: process.env.SESSION_SECRET, //segredo usado para criptografar o cookie
+  saveUninitialized:true, 
+  cookie: { expires: new Date(Date.now() + (3600*1000)), maxAge: 3600*1000 }, //tempo de duração do cookie, em ms. 
+  resave: false
+}))
 app.use(express.json());
-app.use(router)
+app.use(express.urlencoded({ extended: true }))
+
+
+//----------ROTAS----------
+//rota publica (Bem vindo)
+app.get('/', (request, response) => {
+  return response.status(200).json({msg: "Bem vindo a API"})
+})
+
+//rota publica (cadastro de usuario)
+app.post('/users', (request, response) => {
+  return createUserController.handle(request, response);
+});
+
+//rota privada (teste)
 
 export { app };
